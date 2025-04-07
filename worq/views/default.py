0cdf33662e2ccf8cb3e38a04d62c653a087b58a1
Fernando <basura.fgr@gmail.com>
2025-04-07 13:50:22 -0600

    Draggable left sidebar and icons color correction

--- worq/views/default.py
+++ worq/views/default.py
@@ -1,31 +1,17 @@
 from pyramid.view import view_config
-from pyramid.response import Response
-from sqlalchemy.exc import SQLAlchemyError
 
-from .. import models
-
-
-@view_config(route_name='home', renderer='worq:templates/mytemplate.jinja2')
+@view_config(route_name='home', renderer='worq:templates/workq_main.jinja2')
 def my_view(request):
-    try:
-        query = request.dbsession.query(models.MyModel)
-        one = query.filter(models.MyModel.name == 'one').one()
-    except SQLAlchemyError:
-        return Response(db_err_msg, content_type='text/plain', status=500)
-    return {'one': one, 'project': 'WorQ'}
-
-
-db_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
-
-1.  You may need to initialize your database tables with `alembic`.
-    Check your README.txt for descriptions and try to run it.
-
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
-
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
+    #projects = DBSession.query(Project).all()
+    #active_project_id = request.params.get("project_id")
+    projects = {
+        1: {"name": "Project A", "id": "1"},
+        2: {"name": "Project B", "id": "2"},
+        3: {"name": "Project C", "id": "3"},
+    }
+    # Simulating a database query
+    active_project_id = 1
+    return {
+        "projects": projects,
+        "active_project_id": active_project_id
+    }
