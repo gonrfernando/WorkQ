c5054f487a2e686c79897cbd43e7a25e36125176
Joan-omg <johodeltoro@gmail.com>
2025-04-10 12:02:08 -0600

    Se cambiaron el nombre de la ruta de los assets

--- worq/views/sign_in.py
+++
@@ -1,4 +0,0 @@
-from pyramid.view import view_config
-@view_config(route_name='sign_in', renderer='templates/sign_in.jinja2')
-def sign_in_view(request):
-    return {}
--- worq/views/sign_up.py
+++
@@ -1,4 +0,0 @@
-from pyramid.view import view_config
-@view_config(route_name='sign_up', renderer='templates/sign_up.jinja2')
-def sign_up_view(request):
-    return {}
