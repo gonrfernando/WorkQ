978558f67c04aa41edda7c9333ac630d5b4f46a6
Fernando <basura.fgr@gmail.com>
2025-04-14 13:53:54 -0600

    Fixed start page images

--- worq/views/add_user.py
+++ worq/views/add_user.py
@@ -1,6 +1,7 @@
 from pyramid.view import view_config
+from worq.models.models import Projects
 
-@view_config(route_name='add_user', renderer='templates/add_user.jinja2')
+@view_config(route_name='add_user', renderer='templates/test.jinja2')
 def add_user_test_view(request):
     departments = [
         {'id': 1, 'name': 'Engineering'},
@@ -12,7 +13,13 @@ def add_user_test_view(request):
         {'id': 2, 'name': 'User'},
         {'id': 3, 'name': 'Guest'}
     ]
+    projects = request.dbsession.query(Projects).all()
+    json_projects = [{"id": project.id, "name": project.name} for project in projects]
+    active_project_id = request.params.get("project_id")
+    active_project_id = 1
     return {
+        "projects": json_projects,
+        "active_project_id": active_project_id,
         'departments': departments,
         'roles': roles
     }
