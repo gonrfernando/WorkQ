594eb26b88eff9c16c4b0d39cc71e0a6109dd797
Joan-omg <johodeltoro@gmail.com>
2025-04-16 00:06:21 -0600

    Se agrego la vista del PM al igual que correcciones en vistas pasadas

--- worq/routes.py
+++ worq/routes.py
@@ -5,3 +5,4 @@ def includeme(config):
     config.add_route('sign_in', '/sign-in')
     config.add_route('sign_up', '/sign-up')
     config.add_route('add_user', '/add_user')
+    config.add_route('pm_main', '/pm_main')
