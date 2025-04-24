ca514e237609af394d4b6e867602cebb502c57ba
Erick Silva <a21301031@ceti.mx>
2025-04-24 14:15:20 -0600

    user_info responsive

--- worq/routes.py
+++ worq/routes.py
@@ -5,8 +5,5 @@ def includeme(config):
     config.add_route('sign_in', '/sign-in')
     config.add_route('sign_up', '/sign-up')
     config.add_route('add_user', '/add_user')
-<<<<<<< HEAD
     config.add_route('pm_main', '/pm_main')
-=======
     config.add_route('info_user', '/info_user')
->>>>>>> user-info
